a609c3a674e8c419db2c8d7140c4da7530709cf1
marizoo <marizoo@meow.woof>
2021-12-29 20:17:40 +0700

    phase: 10%

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, {useState} from 'react'
+import './App.css'
+import ReadOnlyRow from './components/ReadOnlyRow'
+import data from './mock-data.json'
 
 const App = () => {
+  const [contacts, setContacts] = useState(data)
+
   return (
-    <div>
-      
+    <div className='app-container'>
+      <form>
+        <table>
+
+          <thead>
+            <th>Name</th>
+            <th>Address</th>
+            <th>Phone Number</th>
+            <th>Email</th>
+            <th>Actions</th>
+          </thead>
+
+          <tbody>
+            {contacts.map(contact => (
+              <ReadOnlyRow contact={contact}/>
+            ))}
+          </tbody>
+
+        </table>
+      </form>
     </div>
   )
 }
